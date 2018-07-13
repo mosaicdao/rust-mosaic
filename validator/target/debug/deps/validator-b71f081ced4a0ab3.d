bf50bfe0a0973e5a2bb5b278d12016e54194394b
Martin Schenck <martin@ost.com>
2018-07-13 15:11:21 +0200

    Initial commit with empty projects

---
+++ validator/target/debug/deps/validator-b71f081ced4a0ab3.d
@@ -0,0 +1,5 @@
+/Users/martin/Projects/openst/openst-mosaic/validator/target/debug/deps/validator-b71f081ced4a0ab3: src/main.rs
+
+/Users/martin/Projects/openst/openst-mosaic/validator/target/debug/deps/validator-b71f081ced4a0ab3.d: src/main.rs
+
+src/main.rs:
---
+++ validator/target/debug/validator.d
@@ -0,0 +1,1 @@
+/Users/martin/Projects/openst/openst-mosaic/validator/target/debug/validator.dSYM: /Users/martin/Projects/openst/openst-mosaic/validator/src/main.rs
