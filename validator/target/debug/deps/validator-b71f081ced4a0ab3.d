502f8b83fd834610cc8880199d96d9c7d13a9b18
Martin Schenck <martin@ost.com>
2018-08-06 12:13:35 +0200

    Replaced repo to include only rust-mosaic

--- validator/target/debug/deps/validator-b71f081ced4a0ab3.d
+++
@@ -1,5 +0,0 @@
-/Users/martin/Projects/openst/openst-mosaic/validator/target/debug/deps/validator-b71f081ced4a0ab3: src/main.rs
-
-/Users/martin/Projects/openst/openst-mosaic/validator/target/debug/deps/validator-b71f081ced4a0ab3.d: src/main.rs
-
-src/main.rs:
--- validator/target/debug/validator.d
+++
@@ -1,1 +0,0 @@
-/Users/martin/Projects/openst/openst-mosaic/validator/target/debug/validator.dSYM: /Users/martin/Projects/openst/openst-mosaic/validator/src/main.rs
